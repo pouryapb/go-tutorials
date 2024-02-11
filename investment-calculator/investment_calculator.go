c4a347d550948c7f83b6b7e8fcacf746d2d45549
pouryapb <pouryapb77@gmail.com>
2024-02-11 19:52:16 +0330

    I like this more

--- investment-calculator/investment_calculator.go
+++ investment-calculator/investment_calculator.go
@@ -30,5 +30,5 @@ func main() {
 func calculateFutureValues(investmentAmount, expectedReturnRate, years float64) (fv float64, rfv float64) {
 	fv = investmentAmount * math.Pow(1+expectedReturnRate/100, years)
 	rfv = fv / math.Pow(1+inflationRate/100, years)
-	return
+	return fv, rfv
 }
