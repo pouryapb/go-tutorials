5000ee1e6a9c5dd16e50ea1271e4d54f5933db08
pouryapb <pouryapb77@gmail.com>
2024-02-12 23:58:09 +0330

    struct tags

--- note/note.go
+++ note/note.go
@@ -10,9 +10,9 @@ import (
 )
 
 type Note struct {
-	Title     string
-	Content   string
-	CreatedAt time.Time
+	Title     string    `json:"title"`
+	Content   string    `json:"content"`
+	CreatedAt time.Time `json:"created_at"`
 }
 
 func New(title, content string) (Note, error) {
