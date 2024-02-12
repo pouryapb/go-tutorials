e2d1eade501f766616dd4eb1e128d7ec9cb710b3
pouryapb <pouryapb77@gmail.com>
2024-02-12 18:14:55 +0330

    split code across files

--- main.go
+++ main.go
@@ -44,11 +44,7 @@ func main() {
 	fmt.Println("Welcome to Go Bank!")
 
 	for {
-		fmt.Println("What do you want to do?")
-		fmt.Println("1. Check balance")
-		fmt.Println("2. Deposit money")
-		fmt.Println("3. Withdraw money")
-		fmt.Println("4. Exit")
+		presentOptions()
 
 		var choice int
 		fmt.Print("Your choice: ")
