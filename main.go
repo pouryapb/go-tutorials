36e95b8ce954361f826bab8ed0949ecd1fe7f53c
pouryapb <pouryapb77@gmail.com>
2024-02-12 23:24:42 +0330

    note struct and refactors

--- main.go
+++ main.go
@@ -1,12 +1,14 @@
 package main
 
 import (
-	"errors"
 	"fmt"
+
+	"github.com/pouryapb/go-tutorials/note-app/note"
 )
 
 func main() {
-	title, content, err := getNoteData()
+	title, content := getNoteData()
+	userNote, err := note.New(title, content)
 
 	if err != nil {
 		fmt.Println(err)
@@ -14,30 +16,16 @@ func main() {
 	}
 }
 
-func getNoteData() (string, string, error) {
-	title, err := getUserInput("Note title:")
-
-	if err != nil {
-		return "", "", err
-	}
-
-	content, err := getUserInput("Note content:")
-
-	if err != nil {
-		return "", "", err
-	}
-
-	return title, content, nil
+func getNoteData() (string, string) {
+	title := getUserInput("Note title:")
+	content := getUserInput("Note content:")
+	return title, content
 }
 
-func getUserInput(prompt string) (string, error) {
+func getUserInput(prompt string) string {
 	fmt.Print(prompt)
 	var value string
 	fmt.Scanln(&value)
 
-	if value == "" {
-		return "", errors.New("invalid input")
-	}
-
-	return value, nil
+	return value
 }
