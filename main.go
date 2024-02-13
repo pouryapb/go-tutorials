421fce8c63f62a149e5f9a5966355f80b796f547
pouryapb <pouryapb77@gmail.com>
2024-02-13 22:18:37 +0330

    closure, I see

--- main.go
+++ main.go
@@ -5,10 +5,10 @@ import "fmt"
 func main() {
 	numbers := []int{1, 2, 3, 4}
 	floatNumbers := []float64{1.3, 2.24, 3.10, 4.96}
-	double := transformNumbers(&numbers, getTransformerFunction())
-	fmt.Println(double)
-	trip := transformNumbers(&floatNumbers, triple)
-	fmt.Println(trip)
+	doubled := transformNumbers(&numbers, getTransformerFunction())
+	fmt.Println(doubled)
+	trippled := transformNumbers(&floatNumbers, createTransformer(3.0))
+	fmt.Println(trippled)
 
 	quadrouple := transformNumbers(&numbers, func(num int) int {
 		return num * 4
@@ -31,13 +31,11 @@ func transformNumbers[T int | float32 | float64](numbers *[]T, transfrom func(T)
 }
 
 func getTransformerFunction() func(int) int {
-	return double
+	return createTransformer(2)
 }
 
-func double(number int) int {
-	return number * 2
-}
-
-func triple(number float64) float64 {
-	return number * 3
+func createTransformer[T int | float64](factor T) func(T) T {
+	return func(number T) T {
+		return number * factor
+	}
 }
