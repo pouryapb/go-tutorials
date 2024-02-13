f485ca832830e5aa4d20da15039072388780828f
pouryapb <pouryapb77@gmail.com>
2024-02-13 20:04:25 +0330

    make & finally for

--- maps.go
+++ maps.go
@@ -14,7 +14,18 @@ func main() {
 	websites["ic"] = "https://isee.com"
 	fmt.Println(websites)
 
-	delete(websites, "Google")
-	delete(websites, "Amazon Web Services")
 	fmt.Println(websites)
+
+	list := make([]float64, 3, 5)
+	list[0] = 3
+	list[1] = 1
+	list[2] = 6
+
+	for i, val := range list {
+		fmt.Printf("Index: %v, Value: %v\n", i, val)
+	}
+
+	for key, val := range websites {
+		fmt.Printf("Key: %v\nValue: %v\n", key, val)
+	}
 }
