e4e90eaebf4f3731cb4a9571f7b35addb40721ad
pouryapb <pouryapb77@gmail.com>
2024-02-14 15:27:45 +0330

    out sourcing to a package

--- main.go
+++ main.go
@@ -1,20 +1,14 @@
 package main
 
-import "fmt"
+import (
+	"github.com/pouryapb/go-tutorials/price-calc/prices"
+)
 
 func main() {
-	prices := []float64{10, 20, 30}
 	taxRates := []float64{0, 0.07, 0.1, 0.15}
 
-	result := make(map[float64][]float64)
-
 	for _, taxRate := range taxRates {
-		taxIncludedPrices := make([]float64, len(prices))
-		for priceIndex, price := range prices {
-			taxIncludedPrices[priceIndex] = price * (1 + taxRate)
-		}
-		result[taxRate] = taxIncludedPrices
+		priceJob := prices.NewTaxIncludedPriceJob(taxRate)
+		priceJob.Process()
 	}
-
-	fmt.Println(result)
 }
---
+++ prices/prices.go
@@ -0,0 +1,26 @@
+package prices
+
+import "fmt"
+
+type TaxIncludedPriceJob struct {
+	TaxRate           float64
+	InputPrices       []float64
+	TaxIncludedPrices map[string]float64
+}
+
+func NewTaxIncludedPriceJob(taxRate float64) *TaxIncludedPriceJob {
+	return &TaxIncludedPriceJob{
+		TaxRate:     taxRate,
+		InputPrices: []float64{10, 20, 30},
+	}
+}
+
+func (job TaxIncludedPriceJob) Process() {
+	result := make(map[string]float64)
+
+	for _, price := range job.InputPrices {
+		result[fmt.Sprintf("%.2f", price)] = price * (1 + job.TaxRate)
+	}
+
+	fmt.Println(result)
+}
