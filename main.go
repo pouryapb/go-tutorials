b8816997960fdab2008da8278141a1243cf523b5
pouryapb <pouryapb77@gmail.com>
2024-02-14 17:42:39 +0330

    this is kinda dumb

--- main.go
+++ main.go
@@ -17,15 +17,18 @@ func slowGreet(phrase string, doneChan chan bool) {
 }
 
 func main() {
-	done := make(chan bool)
+	dones := make([]chan bool, 4)
 
-	go greet("Nice to meet you!", done)
-	go greet("How are you?", done)
-	go slowGreet("How ... are ... you ...?", done)
-	go greet("I hope you're liking the course!", done)
+	dones[0] = make(chan bool)
+	go greet("Nice to meet you!", dones[0])
+	dones[1] = make(chan bool)
+	go greet("How are you?", dones[1])
+	dones[2] = make(chan bool)
+	go slowGreet("How ... are ... you ...?", dones[2])
+	dones[3] = make(chan bool)
+	go greet("I hope you're liking the course!", dones[3])
 
-	<-done
-	<-done
-	<-done
-	<-done
+	for _, done := range dones {
+		<-done
+	}
 }
