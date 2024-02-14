42ea14c7d57f9ff19bc8f80fcf45d9556acfba16
pouryapb <pouryapb77@gmail.com>
2024-02-14 19:42:45 +0330

    gin

--- main.go
+++ main.go
@@ -1,5 +1,21 @@
 package main
 
-func main() {
+import (
+	"net/http"
 
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+	server := gin.Default()
+
+	server.GET("/events", getEvents)
+
+	server.Run(":8080")
+}
+
+func getEvents(ctx *gin.Context) {
+	ctx.JSON(http.StatusOK, gin.H{
+		"message": "hi bish",
+	})
 }
