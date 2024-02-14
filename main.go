6ba47d5e2bb41e8106fe0eccd3e02e64b7cf22a3
pouryapb <pouryapb77@gmail.com>
2024-02-14 17:43:20 +0330

    this is better, and I SEEEE

--- main.go
+++ main.go
@@ -14,21 +14,17 @@ func slowGreet(phrase string, doneChan chan bool) {
 	time.Sleep(3 * time.Second) // simulate a slow, long-taking task
 	fmt.Println("Hello!", phrase)
 	doneChan <- true
+	close(doneChan)
 }
 
 func main() {
-	dones := make([]chan bool, 4)
+	done := make(chan bool)
 
-	dones[0] = make(chan bool)
-	go greet("Nice to meet you!", dones[0])
-	dones[1] = make(chan bool)
-	go greet("How are you?", dones[1])
-	dones[2] = make(chan bool)
-	go slowGreet("How ... are ... you ...?", dones[2])
-	dones[3] = make(chan bool)
-	go greet("I hope you're liking the course!", dones[3])
+	go greet("Nice to meet you!", done)
+	go greet("How are you?", done)
+	go slowGreet("How ... are ... you ...?", done)
+	go greet("I hope you're liking the course!", done)
 
-	for _, done := range dones {
-		<-done
+	for range done {
 	}
 }
