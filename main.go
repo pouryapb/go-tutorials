e1c4ed1971341a720153dbacd99815fc771239ed
pouryapb <pouryapb77@gmail.com>
2024-02-14 17:40:35 +0330

    I see

--- main.go
+++ main.go
@@ -9,14 +9,17 @@ func greet(phrase string) {
 	fmt.Println("Hello!", phrase)
 }
 
-func slowGreet(phrase string) {
+func slowGreet(phrase string, doneChan chan bool) {
 	time.Sleep(3 * time.Second) // simulate a slow, long-taking task
 	fmt.Println("Hello!", phrase)
+	doneChan <- true
 }
 
 func main() {
-	greet("Nice to meet you!")
-	greet("How are you?")
-	slowGreet("How ... are ... you ...?")
-	greet("I hope you're liking the course!")
+	// go greet("Nice to meet you!")
+	// go greet("How are you?")
+	done := make(chan bool)
+	go slowGreet("How ... are ... you ...?", done)
+	// go greet("I hope you're liking the course!")
+	<-done
 }
