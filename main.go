3b195eeefb26e5939467cab3b55645266fc3f554
pouryapb <pouryapb77@gmail.com>
2024-02-15 15:45:17 +0330

    db baby

--- main.go
+++ main.go
@@ -19,7 +19,11 @@ func main() {
 }
 
 func getEvents(ctx *gin.Context) {
-	events := models.GetAllEvents()
+	events, err := models.GetAllEvents()
+	if err != nil {
+		ctx.JSON(http.StatusInternalServerError, gin.H{"message": "Could not fetch events."})
+		return
+	}
 	ctx.JSON(http.StatusOK, events)
 }
 
@@ -34,7 +38,12 @@ func createEvent(ctx *gin.Context) {
 
 	event.ID = 1
 	event.UserID = 1
-	event.Save()
+	err = event.Save()
+
+	if err != nil {
+		ctx.JSON(http.StatusInternalServerError, gin.H{"message": "Could not create event."})
+		return
+	}
 
 	ctx.JSON(http.StatusCreated, gin.H{"message": "Event created!", "event": event})
 }
--- models/event.go
+++ models/event.go
@@ -1,9 +1,13 @@
 package models
 
-import "time"
+import (
+	"time"
+
+	"github.com/pouryapb/go-tutorials/rest-api/db"
+)
 
 type Event struct {
-	ID          int
+	ID          int64
 	Name        string    `binding:"required"`
 	Description string    `binding:"required"`
 	Location    string    `binding:"required"`
@@ -11,13 +15,48 @@ type Event struct {
 	UserID      int
 }
 
-var events = []Event{}
+func (e *Event) Save() error {
+	query := `INSERT INTO events(name, description, location, dateTime, user_id)
+	VALUES (?, ?, ?, ?, ?)`
+	statement, err := db.Database.Prepare(query)
 
-func (e Event) Save() {
-	// todo: store in db
-	events = append(events, e)
+	if err != nil {
+		return err
+	}
+
+	defer statement.Close()
+
+	res, err := statement.Exec(e.Name, e.Description, e.Location, e.DateTime, e.UserID)
+
+	if err != nil {
+		return err
+	}
+
+	id, err := res.LastInsertId()
+	e.ID = id
+
+	return err
 }
 
-func GetAllEvents() []Event {
-	return events
+func GetAllEvents() ([]Event, error) {
+	query := "SELECT * FROM events"
+	rows, err := db.Database.Query(query)
+
+	if err != nil {
+		return nil, err
+	}
+	defer rows.Close()
+
+	var events []Event
+
+	for rows.Next() {
+		var event Event
+		err := rows.Scan(&event.ID, &event.Name, &event.Description, &event.Location, &event.DateTime, &event.UserID)
+		if err != nil {
+			return nil, err
+		}
+		events = append(events, event)
+	}
+
+	return events, nil
 }
